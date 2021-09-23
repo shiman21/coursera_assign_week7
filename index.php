3fbf7f16d2a31ab74fd9ebf02d0e102224039f04
shiman21 <86727276+shiman21@users.noreply.github.com>
2021-09-23 21:49:55 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Shimonee Singh - Request/Response Cycle</title>
+</head>
+<body>
+<h1>Shimonee Singh Request / Response</h1>
+<p>The SHA256 hash of "Shimonee Singh" is
+    <?php
+          print hash('sha256', 'Shimonee Singh');
+     ?>
+</p>
+<pre>ASCII ART:
+
+    ***********
+    **       **
+    **
+    ***********
+             **
+    **       **
+    ***********
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+
+      
